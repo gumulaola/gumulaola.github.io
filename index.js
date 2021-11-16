7b768f58e3f378313e3439617a3911e77bedd85d
gumulaola <fx96@stu.pku.edu.cn>
2021-11-16 22:21:51 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+function getCanvas() {
+    let canvas = document.createElement("canvas");
+    canvas.height = 60;
+    canvas.width = 400;
+    let canvasContext = canvas.getContext("2d");
+    canvas.style.display = "inline";
+    canvasContext.textBaseline = "alphabetic";
+    canvasContext.fillStyle = "#f60";
+    canvasContext.fillRect(125, 1, 62, 20);
+    canvasContext.fillStyle = "#069";
+    canvasContext.font = "llpt no-real-font-123";
+    canvasContext.fillText("Cwm fjordbank glyphs vext quiz, \ud83d\ude03", 2, 15);
+    canvasContext.fillStyle = "rgba(102,204,0,0.7)";
+    canvasContext.font = "18pt Arial";
+    canvasContext.fillText("Cwm fjordbank glyphs vext quiz, \ud83d\ude03", 4, 45);
+    let canvasData = canvas.toDataURL();
+    return canvasData;
+}
