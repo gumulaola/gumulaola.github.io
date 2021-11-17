21d37238f77dec3221bfac2138496ce95d26d785
gumulaola <fx96@stu.pku.edu.cn>
2021-11-17 15:36:04 +0800

    1st round mdn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function getCanvas() {
 }
 
 async function getBarcodeSupportedFormats() {
-    let formats = "null";
+    let formats = "0";
     if ('BarcodeDetector' in window && BarcodeDetector.getSupportedFormats != undefined) {
         formats = await BarcodeDetector.getSupportedFormats();
         formats = formats.join();
@@ -28,7 +28,7 @@ async function getBarcodeSupportedFormats() {
 }
 
 async function getBluetooth() {
-    let bluetooth = "null";
+    let bluetooth = "0";
     if (navigator.bluetooth != undefined && navigator.bluetooth.getAvailability != undefined) {
         bluetooth = await navigator.bluetooth.getAvailability();
         bluetooth = bluetooth.toString();
@@ -36,17 +36,143 @@ async function getBluetooth() {
     return bluetooth;
 }
 
+async function getMediaCapabilities() {
+    let audio = "0";
+    let video = "0";
+    if ("mediaCapabilities" in navigator) {
+        const audioConfig = {
+            type: 'file',
+            audio: {
+                contentType: "audio/ogg",
+                channels: 2,
+                bitrate: 132700,
+                samplerate: 5200
+            }
+        };
+        const videoConfig = {
+            type: 'file',
+            video: {
+                contentType: "video/webm;codecs=vp8",
+                width: 800,
+                height: 600,
+                bitrate: 10000,
+                framerate: 30
+            }
+        };
+        audio = await navigator.mediaCapabilities.decodingInfo(audioConfig);
+        video = await navigator.mediaCapabilities.decodingInfo(videoConfig);
+    }
+    return audio["supported"] + audio["smooth"] + audio["powerEfficient"] + "," + video["supported"] + video["smooth"] + video["powerEfficient"];
+}
+
+async function getMediaDevices() {
+    let devices = "0";
+    if (navigator.mediaDevices != undefined && navigator.mediaDevices.enumerateDevices != undefined) {
+        devices = await navigator.mediaDevices.enumerateDevices();
+        devices = JSON.stringify(devices);
+    }
+    return devices;
+}
+
+function getSupportedMediaConstraints() {
+    let supportedMediaConstraints = "0";
+    if (navigator.mediaDevices != undefined && navigator.mediaDevices.getSupportedConstraints != undefined) {
+        supportedMediaConstraints = navigator.mediaDevices.getSupportedConstraints();
+        supportedMediaConstraints = JSON.stringify(supportedMediaConstraints);
+    }
+    return supportedMediaConstraints;
+}
+
+async function getPermissions() {
+    let permissions = "0";
+    if ("permissions" in navigator) {
+        let res = [];
+        let geolocation = await navigator.permissions.query({ name: 'geolocation' });
+        res.push("geolocation:" + geolocation.state);
+        let notifications = await navigator.permissions.query({ name: 'notifications' });
+        res.push("notifications:" + notifications.state);
+        let push = await navigator.permissions.query(Object.assign({ name: "push" }, { userVisibleOnly: true }));
+        res.push("push:" + push.state);
+        let midi = await navigator.permissions.query(Object.assign({ name: "midi" }, { sysex: true }));
+        res.push("midi:" + midi.state);
+        let camera = await navigator.permissions.query({ name: 'camera' });
+        res.push("camera:" + camera.state);
+        let microphone = await navigator.permissions.query({ name: 'microphone' });
+        res.push("microphone:" + microphone.state);
+        let backgroundSync = await navigator.permissions.query({ name: 'background-sync' });
+        res.push("backgroundSync:" + backgroundSync.state);
+        let accelerometer = await navigator.permissions.query({ name: 'accelerometer' });
+        res.push("accelerometer:" + accelerometer.state);
+        let gyroscope = await navigator.permissions.query({ name: 'gyroscope' });
+        res.push("gyroscope:" + gyroscope.state);
+        let magnetometer = await navigator.permissions.query({ name: 'magnetometer' });
+        res.push("magnetometer:" + magnetometer.state);
+        permissions = res.join();
+    }
+    return permissions;
+}
+
+async function getHidDevices() {
+    let devices = "0";
+    if ("hid" in navigator && navigator.hid.getDevices != undefined) {
+        devices = await navigator.hid.getDevices();
+        devices = JSON.stringify(devices);
+    }
+    return devices;
+}
+
+async function getSpeechVoices() {
+    let voices = "0";
+    if ("speechSynthesis" in window) {
+        function setSpeech() {
+            return new Promise(
+                function (resolve, reject) {
+                    let synth = window.speechSynthesis;
+                    let id;
+                    id = setInterval(() => {
+                        if (synth.getVoices().length !== 0) {
+                            resolve(synth.getVoices());
+                            clearInterval(id);
+                        }
+                    }, 10);
+                }
+            )
+        }
+        let res = await setSpeech();
+        let voicesArr = [];
+        for (const item of res) {
+            voicesArr.push(item.name + " " + item.lang + " " + item.localService + " " + item.default)
+        }
+        voices = voicesArr.join();
+    }
+    return voices;
+}
+
 async function main() {
     let fingerprint = {};
 
-    // canvas
+    // Canvas
     fingerprint["canvas"] = getCanvas();
 
-    // barcode
+    // Barcode
     fingerprint["barcodeSupportedFormats"] = await getBarcodeSupportedFormats();
 
-    // bluetooth
+    // Bluetooth
     fingerprint["bluetooth"] = await getBluetooth();
+
+    // Media
+    fingerprint["mediaCapabilities"] = await getMediaCapabilities();
+    fingerprint["mediaDevices"] = await getMediaDevices();
+    fingerprint["supportedMediaConstraints"] = getSupportedMediaConstraints();
+
+    // permissions
+    fingerprint["permissions"] = await getPermissions();
+
+    // HID devices
+    fingerprint["HidDevices"] = await getHidDevices();
+
+    // Speech
+    fingerprint["speechVoices"] = await getSpeechVoices();
 
     console.log(fingerprint);
 }
