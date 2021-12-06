0801c841e979e03c00bc1eee6ea826ab5a1c17c7
gumulaola <fx96@stu.pku.edu.cn>
2021-12-06 14:06:57 +0800

    yes

--- paintworklet.js
+++ paintworklet.js
@@ -9,6 +9,7 @@ registerPaint("rect", class {
         const colors = [color, '#fff'];
         for (let y = 0; y < geom.height / size; y++) {
             for (let x = 0; x < geom.width / size; x++) {
+                ctx.textBaseline = "alphabetic";
                 ctx.fillStyle = colors[(x + y) % colors.length];
                 ctx.beginPath();
                 ctx.rect(x * size, y * size, size, size);
